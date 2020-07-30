bdcf2a9ecfee03849be0fbd7b6508e3ad7734ed6
Supragya Shrestha <supragyashrestha02@gmail.com>
2020-07-31 01:49:51 +0530

    tweets module done

--- tweets/views.py
+++ tweets/views.py
@@ -1,9 +1,13 @@
 import random
+from django.conf import settings
 from django.http import HttpResponse, Http404, JsonResponse
 from django.shortcuts import render, redirect
+from django.utils.http import is_safe_url
 
 from .models import Tweet
 from .forms import TweetForm
+
+ALLOWED_HOSTS = settings.ALLOWED_HOSTS
 
 # Create your views here.
 
@@ -11,18 +15,29 @@ def home_view(request, *args , **kwargs):
     return render(request, "pages/home.html", context={}, status = 200)
 
 def tweet_create_view(request, *args, **kwargs):
+    #print("ajax",request.is_ajax())
     form = TweetForm(request.POST or None)
+    next_url = request.POST.get("next") or None
+
     if form.is_valid():
         obj = form.save(commit=False)
         #other stuff
         obj.save()
+        if request.is_ajax():
+            return JsonResponse(obj.serialize(), status=201)
+
+        if next_url != None and is_safe_url(next_url, ALLOWED_HOSTS):
+            return redirect(next_url)
         form = TweetForm()
+    if form.errors:
+        if request.is_ajax():
+            return JsonResponse(form.errors, status=400)
     return render(request, 'components/form.html', context={"form": form})
 
 def tweet_list_view(request, *args , **kwargs):
     # REST API View
     qs = Tweet.objects.all()
-    tweets_list = [{"id": x.id, "content": x.content, "likes": random.randint(0,32)} for x in qs]
+    tweets_list = [x.serialize() for x in qs]
     data = {
         "isUser": False,
         "response": tweets_list
