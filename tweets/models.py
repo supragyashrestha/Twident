669b04a61a34f57774ef4484676aa97564d4a5d6
Supragya Shrestha <supragyashrestha02@gmail.com>
2020-07-31 03:19:06 +0530

    Django Admin Created

--- tweets/models.py
+++ tweets/models.py
@@ -1,12 +1,19 @@
 import random
 from django.db import models
+from django.conf import settings
+
+User = settings.AUTH_USER_MODEL
 
 # Create your models here.
 
 class Tweet(models.Model):
     # id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE) # many users can many tweets
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to='images/', blank=True)
+
+    #def __str__(self):
+    #    return self.content
 
     class Meta:
         ordering = ['-id']
