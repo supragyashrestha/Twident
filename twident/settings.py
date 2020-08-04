0479e377cf4289912893eb04d7755ba9768aeb6e
Supragya Shrestha <supragyashrestha02@gmail.com>
2020-08-04 20:14:21 +0530

    user profiles created

--- twident/settings.py
+++ twident/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'tweets',
     'corsheaders',
     'accounts',
+    'profiles',
 ]
 
 MIDDLEWARE = [
--- twident/urls.py
+++ twident/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 
 from accounts.views import (
@@ -29,7 +29,6 @@ from accounts.views import (
 from tweets.views import (
     tweets_list_view,
     tweets_detail_view,
-    tweets_profile_view,
     )
 
 urlpatterns = [
@@ -39,7 +38,7 @@ urlpatterns = [
     path('logout/', logout_view),
     path('register/', register_view),
     path('<int:tweet_id>', tweets_detail_view),
-    path('profile/<str:username>', tweets_profile_view),
+    re_path(r'profiles?/', include('profiles.urls')),
     path('api/tweets/', include('tweets.api.urls'))
 ]
 
